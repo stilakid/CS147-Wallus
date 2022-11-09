ae4ec3c5214b1674efaf7b880d50c80b36fb9778
Juben Rana <stilakid@stanford.edu>
2022-11-09 15:18:05 -0800

    Project Setup
    Added packages and libraries for:
    - navigators
    - drawer navigator
    - bottom tab navigator
    - material top tab navigator
    
    Removed node_modules folder

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import 'react-native-gesture-handler';
+// The statement above must be at the absolute top of the entry file (app.js)
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
