43ecce9e2d8b24204f9a7357f788e2935b8c88fd
Juben Rana <stilakid@stanford.edu>
2022-12-01 06:18:48 -0800

    Sync

--- app/screens/BottomTabScreens/homeStack.js
+++ app/screens/BottomTabScreens/homeStack.js
@@ -2,7 +2,6 @@ import { StyleSheet, Text, View } from "react-native";
 import { Themes } from "../../../assets/themes";
 import { AppText } from "../../components/CustomText/customText";
 import { AppButton } from "../../components/Buttons/buttons";
-import { Divider } from "../../components/Divider/divider";
 
 
 // Navigation Packages
@@ -12,6 +11,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Stack screens
 import NotificationScreen from "../NotificationScreen/notificationScreen";
 import HomeScreen from "../HomeScreen/homeScreen";
+import InvitationScreen from "../InvitationScreen/invitationScreen";
 import Card from "../../components/Cards/RationaleCard"
 
 export default function HomeStack({navigation, route}) {
@@ -24,7 +24,7 @@ export default function HomeStack({navigation, route}) {
         <Stack.Navigator>
             <Stack.Screen name="Home Screen" component={HomeScreen} options={{headerShown: false}} />
             <Stack.Screen name="Notifications" component={NotificationScreen} options={{headerShown: false}} />
-            <Stack.Screen name="Card" component={Card} />
+            <Stack.Screen name="Invitation" component={InvitationScreen} options={{headerShown: false}} />
         </Stack.Navigator>
     );
 }
---
+++ app/screens/InvitationScreen/invitationScreen.js
@@ -0,0 +1,68 @@
+import { Themes } from "../../../assets/themes"
+
+// Components
+import { StyleSheet, SafeAreaView, Text, View, SectionList } from "react-native";
+import { AppText } from "../../components/CustomText/customText";
+import { AppButton } from "../../components/Buttons/buttons";
+import { Divider } from "../../components/Divider/divider"
+import Header from "../../components/Header/header";
+
+// Lucide Icons
+import { ChevronLeft } from 'lucide-react-native';
+
+{/* <Home color="black" size={24} />; */}
+export default function InvitationScreen({navigation, route}) {
+
+    // const {  } = route.params;
+
+    const DATA = [
+        {
+        title: 'Invitations',
+        data: ['Pizza', 'Burger', 'Risotto'],
+        },
+        {
+        title: 'Wallus reminders',
+        data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+        },
+        {
+        title: 'Drinks',
+        data: ['Water', 'Coke', 'Beer'],
+        },
+        {
+        title: 'Desserts',
+        data: ['Cheese Cake', 'Ice Cream'],
+        },
+    ];
+
+
+    
+    return(
+        <SafeAreaView style={styles.container}>
+            <Header text="Notifications" navigation={navigation} hasDivider={true} />
+            <SectionList
+                sections={DATA}
+                keyExtractor={(item, index) => item + index} /* unique key for each item */
+                renderItem={({ item }) => <View><Text>{item}</Text></View>} /* render each item as a MenuItem component with the given name */
+                renderSectionHeader={({ section: { title } }) => ( /* grab the title property from each section element */
+                    <View><Text>{title}</Text></View> /* render each section header as a Text component displaying the title */
+                )}
+            />
+        </SafeAreaView>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Themes.colors.white,
+        alignItems: "center",
+        flex: 1,
+      },
+});
+
+
+
+// Branch
+
+// Main
+// Main Branch How is it going??
