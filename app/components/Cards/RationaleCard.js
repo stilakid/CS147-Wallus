1071fd42be733445ca1fbafedf89a5f98322dc46
madifan <madifan@stanford.edu>
2022-12-04 12:53:26 -0800

    Updated Rationale Cards

--- app/components/Cards/RationaleCard.js
+++ app/components/Cards/RationaleCard.js
@@ -39,11 +39,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
     gap: 12,
-    position: 'absolute',
-    color: Themes.colors.neutral_100,
+    backgroundColor: Themes.colors.neutral_100,
     borderRadius: 16,
     width: 358,
-    height: 79
+    height: 80
   },
   container: {
     flexDirection: 'column',
