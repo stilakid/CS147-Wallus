3cd9d90b5fbd898fab6b6065f4c11ca64e8ce832
madifan <madifan@stanford.edu>
2022-12-07 15:04:55 -0800

    Imported company logo assets + group pic image

---
+++ assets/stockData/dailyMovers.js
@@ -0,0 +1,25 @@
+import { Images } from "../themes";
+
+export default{
+    tesla:{
+        companyName: 'Tesla',
+        logoURL: Images.company.tesla,
+        status: 'unstable',
+        stockPrice : 'USD 10.00',
+        stockInfo: 'Tesla, Inc. is an American multinational automotive and clean energy company headquartered in Austin, Texas. Tesla designs and manufactures electric vehicles, battery energy storage from home to grid-scale, solar panels and solar roof tiles, and related products and services.'
+    },
+    amazon:{
+        companyName: 'Amazon',
+        logoURL: Images.company.amazon,
+        status: 'stable',
+        stockPrice : 'USD 10.00',
+        stockInfo: 'Amazon.com, Inc. is an American multinational technology company focusing on e-commerce, cloud computing, online advertising, digital streaming, and artificial intelligence.'
+    },
+    paypal:{
+        companyName: 'Paypal',
+        logoURL: Images.company.paypal,
+        status: 'stable',
+        stockPrice : 'USD 72.00',
+        stockInfo: 'PayPal Holdings, Inc. is an American multinational financial technology company operating an online payments system in the majority of countries that support online money transfers, and serves as an electronic alternative to traditional paper methods such as checks and money orders.'
+    }
+};
