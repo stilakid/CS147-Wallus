60c765f6faa1c22bb14a10b549fcd4c46a7a1cc9
madifan <madifan@stanford.edu>
2022-12-09 14:58:05 -0800

    updated text input placeholder

--- app/screens/InviteFriend/confirmationJenny.js
+++ app/screens/InviteFriend/confirmationJenny.js
@@ -53,7 +53,7 @@ export default function ConfirmationJenny({navigation, route}) {
                     onChangeText={setInput}
                     onPressOut={() => {Keyboard.dismiss()}}
                     value={input}
-                    placeholder="Write down any unlisted reasons..."
+                    placeholder="Write an invitation message to your friends"
                     keyboardType="default"
                     placeholderTextColor={Themes.colors.neutral_500}
                 />
