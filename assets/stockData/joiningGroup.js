ea295e93dc1ee89ffaa49ec8c368f7f074772206
Xinyi Wang <70211672+xinyi-wangxy@users.noreply.github.com>
2022-12-08 22:30:00 -0800

    fix cong

--- assets/stockData/joiningGroup.js
+++ assets/stockData/joiningGroup.js
@@ -98,5 +98,53 @@ export default {
         }
 
     },
-    
+    friendlyBanana:{
+        groupName:'Friendly Bananas',
+        companyName: 'Tesla',
+        groupPicURL: Images.groupProfile.banana,
+        memberPicURL: Images.groupMembers.three,
+        status: 'stable',
+        logoURL: Images.company.tesla,
+        stockInfo: "The fund employs an indexing investment approach designed to track the performance of the index, which represents approximately 100% of the investable U.S. stock market and includes large-, mid-, small-, and micro-cap stocks. It invests by sampling the index, meaning that it holds a broadly diversified collection of securities that, in the aggregate, approximates the full index in terms of key characteristics. ",
+        memberAmount: 5,
+        investAmount: 'USD 20.24',
+        yearTrend: "12.88%",
+        boughtAt: 'USD 20.34',
+        stockPrice: 'USD 40.23',
+        investingFor: '1Y 3M',
+        typicalHold: '4Y 3M',
+        friend: {
+            sharon:{
+                name: 'Sharon',
+                profilePicURL: Images.profilePic.six,
+                dateCommented: 'Oct 16 2022',
+                rationale: "I wanted to invest in something safe.",
+            },
+            justin: {
+                name: 'Justin',
+                profilePicURL: Images.profilePic.seven,
+                dateCommented: 'June 16 2022',
+                rationale: "My friend told me this it's good to start investing with safe index funds!",
+            },
+            alex: {
+                name: 'Alex',
+                profilePicURL: Images.profilePic.three,
+                dateCommented: 'June 4 2022',
+                rationale: "Joined with Avika to have a long term low risk investment!",
+            },
+            steven: {
+                name: 'Steven',
+                profilePicURL: Images.profilePic.four,
+                dateCommented: 'April 16 2022',
+                rationale: "I didn't want to invest in any high risk investments.",
+            },
+            annika: {
+                name: 'Emily',
+                profilePicURL: Images.profilePic.nine,
+                dateCommented: 'April 12 2022',
+                rationale: "I didn't want to invest in any high risk investments.",
+            }
+        }
+
+    }
 }
